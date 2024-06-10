5e18af209ad13bbbd7f4afa41ba009a7a671bca3
0xKev <56137695+0xKev@users.noreply.github.com>
2024-06-10 02:38:23 -0400

    refactor(DevelopmentModelPermission.get_custom_permissions): Return IsAuthenticatedOrReadOnly from rest_framework.permissions if settings.DEBUG is True

--- rent_app/rent_management/permissions.py
+++ rent_app/rent_management/permissions.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from rest_framework.permissions import BasePermission, DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.permissions import BasePermission, IsAuthenticatedOrReadOnly
 
 class DevelopmentModelPermission(BasePermission):
     def has_permission(self, request, view): # if debug is true, anyone can access, else only authenticated users can access through get_custom_permissions
@@ -9,5 +9,5 @@ def get_custom_permissions(request): # if debug is true, use DevelopmentModelPer
     if settings.DEBUG:
         return [DevelopmentModelPermission()]
     else:
-        return [DjangoModelPermissionsOrAnonReadOnly()]
+        return [IsAuthenticatedOrReadOnly()]
     
