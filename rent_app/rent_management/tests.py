d0de3e5777331b01da29459c1910cd5aec3bd80f
0xKev <56137695+0xKev@users.noreply.github.com>
2024-06-10 02:25:19 -0400

    feat(tests.UserTests): Add UserTests to test CustomUser creation

--- rent_app/rent_management/tests.py
+++ rent_app/rent_management/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
 
-# Create your tests here.
+from rest_framework.test import APIClient
+from rest_framework.test import APIRequestFactory
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rent_management.models import *
+from rent_management.views import *
+from rest_framework.test import force_authenticate
+
+
+
+class UserTests(APITestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username="bobbert",
+            email="bob@gmail.com"
+        )
+
+    def test_user_with_username_email_password(self):
+        self.assertIsInstance(self.user, CustomUser)
+        self.assertEqual(self.user.username, "bobbert")
+        self.assertEqual(self.user.email, "bob@gmail.com")
+        
+
